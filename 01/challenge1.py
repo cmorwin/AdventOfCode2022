afe8e21b2932a9b7a8acf4a42e3e730b9a3f9e1f
user <user@user.com>
2022-12-01 20:41:35 -0500

    Fixed bug in challenge1 where the last elf was being ignored. Added alternative method of calculation.

--- 01/challenge1.py
+++ 01/challenge1.py
@@ -1,6 +1,8 @@
 # Challenge Goal: Count the largest amount of Calories carried by a single elf
 #
 #
+# We're going to time a couple methods for fun
+import time
 
 # Read in the file contents
 try:
@@ -16,13 +18,31 @@ elves = contents.split("\n\n")
 # Strip off the final trailing newline (not stripped out by splitting on '\n\n')
 elves[-1] = elves[-1][:-1]
 
+
 # For each elf, add up their food's Calories, only keeping the highest total
+start_time = time.process_time()
 max_calories = 0
-for elf in elves[:-1]:
+for elf in elves:
     foods = [int(i) for i in elf.split("\n")]
     total_calories = sum(foods)
     if total_calories > max_calories:
         max_calories = total_calories
+stop_time = time.process_time()
 
 # Print result
-print(f"Max Calories carried by an elf: {max_calories}")
+print("Method 1: Manual 'if > max' comparisons")
+print(f"  Max Calories carried by an elf: {max_calories}")
+print(f"  Calculation took {(stop_time - start_time)*1000:.4} milliseconds")
+
+
+# For each elf, add up their food's Calories
+start_time = time.process_time()
+for i in range(0, len(elves)):
+    elves[i] = sum([int(i) for i in elves[i].split("\n")])
+max_calories = max(elves)
+stop_time = time.process_time()
+
+# Print result
+print("Method 2: Python's max() function")
+print(f"  Max Calories carried by an elf: {max_calories}")
+print(f"  Calculation took {(stop_time - start_time)*1000:.4} milliseconds")
